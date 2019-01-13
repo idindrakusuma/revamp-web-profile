4aa8ba547c35a49af5e0f1c2ac1433b7dbfa0eae
Indra Kusuma <indrakusuma.udinus@gmail.com>
2019-01-13 16:45:59 +0700

    add unit test cardExperience

---
+++ src/components/CardExperience/__test__/cardExperience.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render } from 'react-testing-library';
+
+import CardExperience from '../index';
+
+const TestCardExperience = <CardExperience title="Software Engineer" date="November 2017" company="Markir"/>;
+
+describe('CardExperience Component', () => {
+  it('Should render correctly', () => {
+    const tree = render(TestCardExperience);
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Should render Title', () => {
+    const { getByText } = render(TestCardExperience);
+    expect(getByText('Software Engineer')).toBeInTheDocument();
+  });
+  
+  it('Should render Date', () => {
+    const { getByText } = render(TestCardExperience);
+    expect(getByText('November 2017')).toBeInTheDocument();
+  });
+
+  it('Should render Company', () => {
+    const { getByText } = render(TestCardExperience);
+    expect(getByText('Markir')).toBeInTheDocument();
+  });
+});
