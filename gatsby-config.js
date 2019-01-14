55cb5b774be075a555013a51b6a5d43e53d7679b
Indra Kusuma <indrakusuma.udinus@gmail.com>
2019-01-14 22:22:32 +0700

    add SEO content

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  plugins: [
-    `gatsby-plugin-emotion`,
-  ]
+  plugins: [`gatsby-plugin-emotion`],
+  siteMetadata: {
+    title: 'Indra Kusuma - Profesional Web Developer',
+    description:
+      'Hello! My name is Indra Kusuma. I am an optimistic and type of person of learn by doing who have an interest in Software Engineering, specifically about Web Development.',
+    url: 'https://indrakusuma.web.id',
+    image: 'https://user-images.githubusercontent.com/10141928/35048258-069b7a48-fbcf-11e7-8c38-c924da4cce81.jpg',
+    twitterUsername: '@id_indrakusuma',
+  },
 };
