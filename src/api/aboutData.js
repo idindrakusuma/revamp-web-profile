09899f16b4aa1e14e39bd71b7090be9f827c0539
Indra Kusuma <indrakusuma.udinus@gmail.com>
2019-01-20 22:37:25 +0700

    add into and mini bio

---
+++ src/api/aboutData.js
@@ -0,0 +1,33 @@
+export const aboutMe = [
+  {
+    question: 'Position',
+    answer: 'Software Engineer Mobile Web at Tokopedia',
+  },
+  {
+    question: 'Email',
+    answer: 'id.indrakusuma@gmail.com',
+  },
+  {
+    question: 'Location',
+    answer: 'Jakarta, Indonesia',
+  },
+];
+
+export const skills = [
+  {
+    skill: 'HTML, CSS',
+    percentage: '90%',
+  },
+  {
+    skill: 'ReactJS, VueJS',
+    percentage: '95%',
+  },
+  {
+    skill: 'Team Work',
+    percentage: '90%',
+  },
+  {
+    skill: 'Design / Inkscape',
+    percentage: '70%',
+  },
+];
--- src/pages/about.js
+++ src/pages/about.js
@@ -3,13 +3,28 @@ import React from 'react';
 import Title from '../components/Title';
 import WithSidebar from '../components/HOC/withSidebar';
 
-import { section } from '../components/Layouts';
+import { section, row, intro, info } from '../components/Layouts';
+import { aboutMe } from '../api/aboutData';
 
 const AboutPage = () => {
   return (
     <WithSidebar>
       <div css={section} id="about-indra">
-        <Title text="About Page" />
+        <Title text="About Indra Kusuma" />
+        <div css={row}>
+          <p css={intro}>
+            Hello! My name is Indra Kusuma. I am an optimistic and type of person of learn by doing who have an interest in Software Engineering, specifically about Web Development.
+          </p>
+        </div>
+        <div css={row}>
+          <ul css={info}>
+            { aboutMe.map(me => (
+              <li>
+                <b>{me.question} :</b> {me.answer}
+              </li>
+            )) }
+          </ul>
+        </div>
       </div>
     </WithSidebar>
   );
